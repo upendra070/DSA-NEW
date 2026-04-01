b289cf78a9ec41635855855f77ea51a705f6983d
UPENDRA KUMAR DUBEY <ud9628@gmail.com>
2026-04-01 20:37:08 +0530

    Add Two_sum.cpp implementing the two-sum algorithm

---
+++ Arrays/Two Pointer/Two_sum.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> map;
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (map.find(complement) != map.end()) {
+            return {map[complement], i};
+        }
+        map[nums[i]] = i;
+    }
+    return {}; // return empty vector if no solution found
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    return 0;
+}
